201996bf327414be2382fe272540686912d63196
Juan Carlos Moreno J <juancmorenoj@gmail.com>
2025-03-08 13:07:28 -0500

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,16 @@
 const Nav = () => {
+  const links = ['English Cards', 'Math Cards'];
+
   return (
     <>
-      <div className="navBar w-full h-16 bg-amber-200"></div>
+      <div className="navBar w-full h-16 bg-amber-200">
+        <ul>
+            {links.map((item) => {
+              return <h2>{item}</h2>;
+                    
+            })}
+        </ul>
+      </div>
     </>
   )
 }
